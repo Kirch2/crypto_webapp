8348338b849f6dc6dcf7b1c55f82d89025a4ff5e
Chris <Kirch2@gmail.com>
2021-10-06 17:53:27 -0400

    register form rendered

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,3 +1,29 @@
+import React from "react";
+
+export function LoginForm(props: { handleLoginSubmit: any }) {
+  return (
+    <form onSubmit={props.handleLoginSubmit}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        name="full_name"
+        placeholder="Please enter your username"
+        className="login-field form-control"
+      />
+      <br />
+
+      <input
+        type="text"
+        name="phone_number"
+        placeholder="Please enter your email"
+        className="login-field form-control"
+      />
+      <br />
+      <input type="submit" className="btn btn-primary" id="login-button" />
+    </form>
+  );
+}
+
 export function Register() {
-  return <p>Register Page</p>;
+  return <LoginForm handleLoginSubmit={console.log} />;
 }
--- client/src/pages/WatchListEdit.tsx
+++ client/src/pages/WatchListEdit.tsx
@@ -1,29 +1,17 @@
-import {
-  WatchListEntry,
-  WatchListEntryFetcher,
-} from "../components/WatchListEntryFetcher";
-import { Loading } from "../components/Loading";
-
-export function WatchListEntryCard(props: { entry: WatchListEntry }) {
-  return <pre>{JSON.stringify(props, null, 4)}</pre>;
-}
+import { WatchListCreator } from "../components/WatchListCreator";
+import { WatchListForm } from "../components/WatchListForm";
 
 export function WatchListEdit() {
   return (
-    <WatchListEntryFetcher>
-      {({ loading, watchlistentries }) => {
-        if (loading) {
-          return <Loading />;
-        }
-
-        return (
-          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-            {watchlistentries.map((entry) => (
-              <WatchListEntryCard entry={entry} key={entry.id} />
-            ))}
-          </div>
-        );
-      }}
-    </WatchListEntryFetcher>
+    <WatchListCreator>
+      {({ createWatchList, loading }) => (
+        <WatchListForm
+          loading={loading}
+          onSubmit={(newWatchList) => {
+            createWatchList(newWatchList);
+          }}
+        />
+      )}
+    </WatchListCreator>
   );
 }
